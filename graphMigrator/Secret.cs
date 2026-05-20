7a3615913afca1097194b79d4114d5ce3bbce6a6
Roman Ghazi <rog001@edu.zealand.dk>
2026-05-21 00:13:23 +0200

    Add global json

---
+++ graphMigrator/Secret.cs
@@ -0,0 +1,10 @@
+﻿namespace graphMigrator;
+
+public class Secret
+{
+    public string MySqlConnectionString { get; set; } =
+        "Server=localhost;Port=3306;Database=mydb;User=root;Password=123456;";
+
+    public string MongoDbConnectionString { get; set; } =
+        "mongodb://admin:secret@localhost:27017/mydatabase?authSource=admin";
+}
