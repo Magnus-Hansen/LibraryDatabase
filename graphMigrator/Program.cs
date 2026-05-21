235c4afe0e222ca57066239d25b82c1bf63feca9
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2026-05-21 13:11:13 +0200

    fix to merge program

--- graphMigrator/Program.cs
+++ graphMigrator/Program.cs
@@ -62,50 +62,53 @@ internal class Program
             await neo4j.Neo4jExecute(transaction, mysql.GetLoans(), neo4jQueries["Loaner_Loan"]);
             await neo4j.Neo4jExecute(transaction, mysql.GetFines(), neo4jQueries["Fine_Loan"]);
             await neo4j.Neo4jExecute(transaction, mysql.GetLoans(), neo4jQueries["Loan_Inventory"]);
-        // MongoDB Migration
-        var mongoDB = new MongoDBService(secret.MongoDbConnectionString, secret.MySqlConnectionString);
 
-        await mongoDB.EnsureUsersAndPrivilegesAsync();
-
-        // Ensure collections exist + create indexes (unique constraints)
-        await mongoDB.EnsureConstraintsAsync();
-
-        using (var session = mongoDB.StartSession())
-        { 
-            var transactionOptions = new TransactionOptions(
-            readConcern: ReadConcern.Majority,
-            writeConcern: WriteConcern.WMajority
-            );
-
-            session.StartTransaction(transactionOptions);
-            try {
-                await mongoDB.ClearCollection("Items");
-                await mongoDB.InsertData("Items", mongoDB.TransformItems());
-                await mongoDB.ClearCollection("Inventory");
-                await mongoDB.InsertData("Inventory", mongoDB.TransformInventory());
-                await mongoDB.ClearCollection("Loaners");
-                await mongoDB.InsertData("Loaners", mongoDB.TransformLoaners());
-                await mongoDB.ClearCollection("Loans");
-                await mongoDB.InsertData("Loans", mongoDB.TransformLoans());
-                await mongoDB.ClearCollection("Reservations");
-                await mongoDB.InsertData("Reservations", mongoDB.TransformReservations());
             foreach (var nodeId in NodeId.Values)
             {
                 await neo4j.Neo4jExecute(transaction, nodeId);
             }
 
-            Console.WriteLine("Completed migration");
+            Console.WriteLine("Completed neo4j migration");
+
+            // MongoDB Migration
+            var mongoDB = new MongoDBService(secret.MongoDbConnectionString, secret.MySqlConnectionString);
+
+            await mongoDB.EnsureUsersAndPrivilegesAsync();
+
+            // Ensure collections exist + create indexes (unique constraints)
+            await mongoDB.EnsureConstraintsAsync();
+
+            using (var session = mongoDB.StartSession())
+            {
+                var transactionOptions = new TransactionOptions(
+                readConcern: ReadConcern.Majority,
+                writeConcern: WriteConcern.WMajority
+                );
+
+                session.StartTransaction(transactionOptions);
+                try
+                {
+                    await mongoDB.ClearCollection("Items");
+                    await mongoDB.InsertData("Items", mongoDB.TransformItems());
+                    await mongoDB.ClearCollection("Inventory");
+                    await mongoDB.InsertData("Inventory", mongoDB.TransformInventory());
+                    await mongoDB.ClearCollection("Loaners");
+                    await mongoDB.InsertData("Loaners", mongoDB.TransformLoaners());
+                    await mongoDB.ClearCollection("Loans");
+                    await mongoDB.InsertData("Loans", mongoDB.TransformLoans());
+                    await mongoDB.ClearCollection("Reservations");
+                    await mongoDB.InsertData("Reservations", mongoDB.TransformReservations());
+
+                    await session.CommitTransactionAsync();
+                    Console.WriteLine("Migration completed successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error during migration: {ex.Message}");
+                    await session.AbortTransactionAsync();
+                    return;
+                }
+            }
         });
     }
-                await session.CommitTransactionAsync();
-                Console.WriteLine("Migration completed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error during migration: {ex.Message}");
-                await session.AbortTransactionAsync();
-                return;
-            }
-        }
-        }
 }
